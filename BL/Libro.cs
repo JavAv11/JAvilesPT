379cf3814ee1d4bbc9e30a3f1b77f4ea9abf2764
JavAv11 <Javcoria01@gmail.com>
2022-12-08 14:56:21 -0600

    Add project files.

---
+++ BL/Libro.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Libro
+    {
+        public static ML.Result Add(ML.Libro libro)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString()))
+                {
+                    using(SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = "INSERT INTO Libro(Nombre, IdAutor, NumeroPaginas, FechaPublicacion, IdEditorial,Edicion,IdGenero)" +
+                            "Values(@Nombre, @IdAutor,@NumeroPaginas,@FechaPublicacion, @IdEditorial, @Edicion,@IdGenero)";
+                        cmd.Connection = context;
+                        SqlParameter[] collection = new SqlParameter[7];
+
+                        collection[0] = new SqlParameter("@Nombre", System.Data.SqlDbType.VarChar);
+                        collection[0].Value = libro.Nombre;
+
+                        collection[1] = new SqlParameter("@IdAutor", System.Data.SqlDbType.Int);
+                        collection[1].Value = libro.IdAutor;
+
+                        collection[2] = new SqlParameter("@NumeroPaginas", System.Data.SqlDbType.VarChar);
+                        collection[2].Value = libro.NumeroPaginas;
+
+                        collection[3] = new SqlParameter("@FechaPublicacion", System.Data.SqlDbType.VarChar);
+                        collection[3].Value = libro.FechaPublicacion;
+
+                        collection[4] = new SqlParameter("@IdEditorial", System.Data.SqlDbType.Int);
+                        collection[4].Value = libro.IdEditorial;
+
+                        collection[5] = new SqlParameter("@Edicion", System.Data.SqlDbType.VarChar);
+                        collection[5].Value = libro.Edicion;
+
+                        collection[6] = new SqlParameter("@IdGenero", System.Data.SqlDbType.Int);
+                        collection[6].Value = libro.IdGenero;
+
+                        cmd.Parameters.AddRange(collection);
+                        cmd.Connection.Open();
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if(rowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage=ex.Message;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ PL/Libro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Libro
+    {
+
+        public static void Add()
+        {
+            ML.Libro libro = new ML.Libro();
+
+
+            Console.WriteLine("Escribe Nombre de un Libro");
+            libro.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe IdAutor de un Libro");
+            libro.IdAutor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe Numero de Paginas de un Libro");
+            libro.NumeroPaginas = Console.ReadLine();
+
+            Console.WriteLine("Escribe Fecha de Publicacion de un Libro");
+            libro.FechaPublicacion = Console.ReadLine();
+
+            Console.WriteLine("Escribe IdEditorial de un Libro");
+            libro.IdEditorial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe Edicion de un Libro");
+            libro.Edicion = Console.ReadLine();
+
+            Console.WriteLine("Escribe IdGenero de un Libro");
+            libro.IdGenero = int.Parse(Console.ReadLine());
+
+            ML.Result result = new ML.Result();
+
+            result = BL.Libro.Add(libro);
+
+            if (result.Correct = true)
+            {
+                Console.WriteLine("Libro Registrado");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Libro no Registrado");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
